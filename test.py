5b2d818110147649add9d967354fcfcfc8f7e62e
Aaditya Prasad Gupta <aadityaprasad.gupta@gmail.com>
2026-03-13 15:35:03 +0530

    Updated the file

--- test.py
+++ test.py
@@ -2,7 +2,9 @@ import psycopg2
 conn = psycopg2.connect(dbname="postgres",user="postgres", password="Gautam",host="localhost",port="5432")
 
 cursor = conn.cursor()
-cursor
+cursor.execute('''create table employees(Name text, ID int, Age int);''')
+print("Table created successfully")
+conn.commit()
+conn.close()
 
 
-print("Connected Successfully")
